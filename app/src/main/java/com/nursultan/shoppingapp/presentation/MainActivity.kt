3181a251863d611d82cf7c430bc4509d4396a6d8
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-03-02 20:58:11 +0300

    imp bottom nav menu in main activity

--- app/src/main/java/com/nursultan/shoppingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/MainActivity.kt
@@ -2,11 +2,38 @@ package com.nursultan.shoppingapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.nursultan.shoppingapp.R
+import com.nursultan.shoppingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setBottomNavItemsListener()
+    }
+
+    private fun setBottomNavItemsListener() {
+        binding.bnav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_setting -> {
+                    Log.d("Nav click", "nav_setting")
+                }
+                R.id.nav_notes -> {
+                    Log.d("Nav click", "nav_notes")
+                }
+                R.id.nav_shopping_list -> {
+                    Log.d("Nav click", "nav_shopping_list")
+                }
+                R.id.nav_new_item -> {
+                    Log.d("Nav click", "nav_new_item")
+                }
+            }
+            true
+        }
     }
 }
