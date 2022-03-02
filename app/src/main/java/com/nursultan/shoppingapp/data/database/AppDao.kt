afa8479f23ba771201994b3c194eeea4b6c3df52
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-03-02 19:37:01 +0300

    added Application database

---
+++ app/src/main/java/com/nursultan/shoppingapp/data/database/AppDao.kt
@@ -0,0 +1,17 @@
+package com.nursultan.shoppingapp.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.nursultan.shoppingapp.data.database.model.NoteItemDbModel
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface AppDao {
+    @Insert
+    suspend fun insertNote(note: NoteItemDbModel)
+
+    @Query("select * from note_list")
+    fun getAllNotes(): Flow<List<NoteItemDbModel>>
+}
