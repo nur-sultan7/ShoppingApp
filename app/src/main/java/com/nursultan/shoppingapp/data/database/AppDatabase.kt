c904b252b9c405874013e2632e54dfeb75044c5b
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-03-17 23:19:15 +0300

    impl menu item (save) onClickListener

--- app/src/main/java/com/nursultan/shoppingapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/nursultan/shoppingapp/data/database/AppDatabase.kt
@@ -12,7 +12,7 @@ import com.nursultan.shoppingapp.data.database.model.ShopListNameItemDbModel
 @Database(
     entities = [LibraryItemDbModel::class, NoteItemDbModel::class,
         ShopListItemDbModel::class, ShopListNameItemDbModel::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
