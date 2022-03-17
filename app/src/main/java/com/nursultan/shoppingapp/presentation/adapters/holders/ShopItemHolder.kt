d8a03eb71ce062bcca068394b3ba448735377d07
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-03-18 02:25:22 +0300

    impl strike text for ShopListItem

--- app/src/main/java/com/nursultan/shoppingapp/presentation/adapters/holders/ShopItemHolder.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/adapters/holders/ShopItemHolder.kt
@@ -1,10 +1,12 @@
 package com.nursultan.shoppingapp.presentation.adapters.holders
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.nursultan.shoppingapp.R
 import com.nursultan.shoppingapp.data.database.model.ShopListItemDbModel
 import com.nursultan.shoppingapp.databinding.ItemLibraryShopListBinding
 import com.nursultan.shoppingapp.databinding.ItemShopListBinding
@@ -17,6 +19,9 @@ class ShopItemHolder(val view: View) : RecyclerView.ViewHolder(view) {
             tvName.text = item.name
             tvInfo.text = item.info
             tvInfo.visibility = VisibilitySetter.setVisibilityByString(item.info)
+            checkBox.setOnClickListener {
+                setPaintFlagAndColor(binding)
+            }
         }
     }
 
@@ -24,6 +29,21 @@ class ShopItemHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val binding = ItemLibraryShopListBinding.bind(view)
     }
 
+    private fun setPaintFlagAndColor(binding: ItemShopListBinding) {
+        binding.apply {
+            if (checkBox.isChecked) {
+                tvName.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                tvInfo.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                tvName.setTextColor(binding.root.context.getColor(R.color.grey))
+                tvInfo.setTextColor(binding.root.context.getColor(R.color.grey))
+            } else {
+                tvName.paintFlags = Paint.ANTI_ALIAS_FLAG
+                tvInfo.paintFlags = Paint.ANTI_ALIAS_FLAG
+                tvName.setTextColor(binding.root.context.getColor(R.color.black))
+                tvInfo.setTextColor(binding.root.context.getColor(R.color.black))
+            }
+        }
+    }
 
 
     companion object {
