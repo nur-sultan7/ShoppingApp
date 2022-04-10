59cefcf910de2c448ce9b8eebbf547c6b0eaa2ee
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-04-10 22:39:48 +0300

    implementation of users billing

--- app/src/main/java/com/nursultan/shoppingapp/billing/BillingManager.kt
+++ app/src/main/java/com/nursultan/shoppingapp/billing/BillingManager.kt
@@ -5,6 +5,11 @@ import com.android.billingclient.api.*
 
 class BillingManager(private val activity: AppCompatActivity) {
     private var bilClient: BillingClient? = null
+
+    init {
+        setUpBillingClient()
+    }
+
     private fun setUpBillingClient() {
         bilClient = BillingClient.newBuilder(activity)
             .setListener(getPurchasesUpdateListener())
@@ -12,10 +17,39 @@ class BillingManager(private val activity: AppCompatActivity) {
             .build()
     }
 
+    fun startConnection() {
+        bilClient?.startConnection(object : BillingClientStateListener {
+            override fun onBillingServiceDisconnected() {
+            }
+
+            override fun onBillingSetupFinished(p0: BillingResult) {
+                getItem()
+            }
+
+        })
+    }
+
+    private fun getItem() {
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_AD)
+        val skuDetailsParams = SkuDetailsParams.newBuilder()
+        skuDetailsParams.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        bilClient?.querySkuDetailsAsync(skuDetailsParams.build()) { bResult, list ->
+            if (bResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                if (!list.isNullOrEmpty()) {
+                    val bilFlow = BillingFlowParams.newBuilder().setSkuDetails(list[0]).build()
+                    bilClient?.launchBillingFlow(activity, bilFlow)
+                }
+            }
+        }
+    }
+
     private fun getPurchasesUpdateListener() =
         PurchasesUpdatedListener { bResult, list ->
             if (bResult.responseCode == BillingClient.BillingResponseCode.OK) {
-
+                list?.get(0)?.let {
+                    nonConsumableItem(it)
+                }
             }
         }
 
@@ -31,5 +65,9 @@ class BillingManager(private val activity: AppCompatActivity) {
                 }
             }
         }
+    }
+
+    companion object {
+        const val REMOVE_AD = "remove_ad"
     }
 }
