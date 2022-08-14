ae558550daefb8df1902ab6dbc6b1742ff8355da
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-08-14 23:47:31 +0300

    implementation viewPager2 connection with tabLayout

--- app/src/main/java/com/nursultan/shoppingapp/presentation/fragments/tabs/FreezerTabFragment.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/fragments/tabs/FreezerTabFragment.kt
@@ -1,10 +1,10 @@
 package com.nursultan.shoppingapp.presentation.fragments.tabs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.nursultan.shoppingapp.R
 
 
