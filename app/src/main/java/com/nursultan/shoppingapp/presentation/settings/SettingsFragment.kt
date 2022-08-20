ae2ac612a1b5212b256785228ae7341e614d0fd3
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-08-20 23:58:54 +0300

    implementation viewPager2 connection with tabLayout

--- app/src/main/java/com/nursultan/shoppingapp/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/settings/SettingsFragment.kt
@@ -1,7 +1,6 @@
 package com.nursultan.shoppingapp.presentation.settings
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
