187d9c70ad462d4d0c59f984f96390b9356c8ba8
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-08-25 23:39:14 +0300

    fix some code

--- app/src/main/java/com/nursultan/shoppingapp/presentation/adapters/utils/ShopListNameDiffUtil.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/adapters/utils/ShopListNameDiffUtil.kt
@@ -3,7 +3,7 @@ package com.nursultan.shoppingapp.presentation.adapters.utils
 import androidx.recyclerview.widget.DiffUtil
 import com.nursultan.shoppingapp.data.database.model.ShopListNameItemDbModel
 
-object ShopListNameDiffUtil: DiffUtil.ItemCallback<ShopListNameItemDbModel>() {
+object ShopListNameDiffUtil : DiffUtil.ItemCallback<ShopListNameItemDbModel>() {
     override fun areItemsTheSame(
         oldItem: ShopListNameItemDbModel,
         newItem: ShopListNameItemDbModel
