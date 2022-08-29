ca961ff44536363e16baf6ae903514a716473e6f
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-08-29 23:59:12 +0300

    fix some code

--- app/src/main/java/com/nursultan/shoppingapp/presentation/fragments/tabs/FreezerTabFragment.kt
+++ app/src/main/java/com/nursultan/shoppingapp/presentation/fragments/tabs/FreezerTabFragment.kt
@@ -13,7 +13,6 @@ class FreezerTabFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_freezer_tab, container, false)
     }
 
